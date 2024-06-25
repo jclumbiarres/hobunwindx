e03c739ee00d86f89a0beb1595d7d887aa341d2a
Juan Carlos Lumbiarres <jclumbiarres@protonmail.com>
2024-06-25 16:59:53 +0200

    fixed: Header links to ALink

--- src/components/core/Header.tsx
+++ src/components/core/Header.tsx
@@ -8,30 +8,21 @@ export const Header = () => {
           url="/home"
           desc="Home"
         />
-        <a
-          href="#"
-          hx-get="/about"
-          hx-target="#root"
-          class="text-white text-lg font-bold"
-        >
-          About
-        </a>
-        <a
-          href="#"
-          hx-get="/contact"
-          hx-target="#root"
-          class="text-white text-lg font-bold"
-        >
-          Contact
-        </a>
-        <a
-          href="#"
-          hx-get="/login"
-          hx-target="#root"
-          class="text-white text-lg font-bold"
-        >
-          Login
-        </a>
+        <ALink
+          className="text-white text-lg font-bold"
+          url="/about"
+          desc="About"
+        />
+        <ALink
+          className="text-white text-lg font-bold"
+          url="/contact"
+          desc="Contact"
+        />
+        <ALink
+          className="text-white text-lg font-bold"
+          url="/login"
+          desc="Login"
+        />
       </nav>
     </header>
   );
