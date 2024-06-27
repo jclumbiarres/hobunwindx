88b5e1b02dd91d652f3fcf26d3e47d23aa393dc3
Juan Carlos Lumbiarres <jclumbiarres@protonmail.com>
2024-06-27 18:20:46 +0200

    feat: Update ALink component to accept a target prop

--- src/components/core/Link.tsx
+++ src/components/core/Link.tsx
@@ -4,11 +4,23 @@ interface ALinkProps {
   className: string;
   url: string;
   desc: string;
+  target?: string;
 }
 
-export const ALink: FC<ALinkProps> = ({ className, url, desc }) => {
+/**
+ * A custom link component.
+ *
+ * @component
+ * @param {Object} props - The component props.
+ * @param {string} props.className - The CSS class name for the link.
+ * @param {string} props.url - The URL for the link.
+ * @param {string} props.desc - The description for the link.
+ * @param {string} [props.target] - The target for the link. Defaults to "#root".
+ * @returns {JSX.Element} The link component.
+ */
+export const ALink: FC<ALinkProps> = ({ className, url, desc, target }) => {
   return (
-    <a className={className} href="#" hx-get={url} hx-target="#root">
+    <a className={className} href="#" hx-get={url} hx-target={target || "#root"}>
       {desc}
     </a>
   );
